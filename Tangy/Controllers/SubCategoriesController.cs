31fa32bc97c58789b2d324280a1429bb4f0f21f2
somto amaugo <amaugosomto@gmail.com>
2020-01-20 16:25:43 +0100

    Add project files.

---
+++ Tangy/Controllers/SubCategoriesController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tangy.Data;
+using Tangy.Models;
+using Tangy.Models.SubCategoryViewModel;
+
+namespace Tangy.Controllers
+{
+    public class SubCategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public SubCategoriesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Get Action
+        public async Task<IActionResult> Index() =>
+            //var subCategories = _db.SubCategory.Include(s => s.Category);
+            View(await _db.SubCategory.Include(s => s.Category).ToListAsync());
+
+        //GET Action for Create
+        public IActionResult Create()
+        {
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = new SubCategory(),
+                SubCategoryList = _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToList()
+            };
+
+            return View(model);
+
+        }
+
+        //POST Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create (SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryEsists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name).Count();
+                var doesSubCatAndCatEsists = _db.SubCategory.Where(s => s.Name == model.SubCategory.Name && s.CategoryId == model.SubCategory.CategoryId).Count();
+
+                if (doesSubCategoryEsists > 0 && model.IsNew)
+                {
+                    //error
+                    StatusMessage = "Error: Sub Category NAme already Exists";
+                }
+                else
+                {
+                    if (doesSubCategoryEsists == 0 && !model.IsNew)
+                    {
+                        //error
+                        StatusMessage = "Error: Sub Category does not exits";
+                    } else
+                    {
+                        if (doesSubCatAndCatEsists > 0)
+                        {
+                            //error
+                            StatusMessage = "Error: Category and Sub Category combination exists";
+                        } 
+                        else
+                        {
+                            _db.Add(model.SubCategory);
+                            await _db.SaveChangesAsync();
+                            return RedirectToAction(nameof(Index));
+                        }
+                    }
+                }
+            }
+
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = _db.Category.ToList(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToList(),
+                StatusMessage = StatusMessage
+            };
+            return View(modelVM);
+        }
+    }
+}
